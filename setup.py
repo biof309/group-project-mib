b3dd8e875ec78b3c1e248c5eeda7ca9f87d9be20
Martin Skarzynski <marskar@gmail.com>
2018-09-21 10:35:37 -0400

    add example project structure for final projects

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A template I recommend for final projects. Feel free to modify as needed!',
+    author='biof309',
+    license='MIT',
+)
